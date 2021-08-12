fbb529c3eca07cbf097bdea8bdb2cb724ea918f5
“kulkarohan” <53792428+kulkarohan@users.noreply.github.com>
2021-08-12 18:40:28 -0400

    Remove preset media and market addresses

--- contracts/Collections.sol
+++ contracts/Collections.sol
@@ -17,9 +17,9 @@ contract Collections {
     // Deployment address
     address private _owner;
     // Zora media address
-    address public mediaContract = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
+    address public mediaContract;
     // Zora market address
-    address public marketContract = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
+    address public marketContract;
 
     // ============ Structs ============
 
